36729cfdbbbeddac6aaff8876a90702bc56e2464
Archibald Gilliocq <archigilliocq@gmail.com>
2024-10-09 22:11:34 +0200

    Création du projet en Angular, mise en place d'un formulaire de facture, création des coordonnées

---
+++ dashboard-v7/src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// dashboard components
+import { LayoutComponent } from './dashboard/layout/layout.component';
+import { TopBarComponent } from './dashboard/top-bar/top-bar.component';
+import { OverlayComponent } from './dashboard/overlay/overlay.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar/sidebar.component';
+import { SidebarItemComponent } from './dashboard/sidebar/sidebar-item/sidebar-item.component';
+import { SidebarItemsComponent } from './dashboard/sidebar/sidebar-items/sidebar-items.component';
+import { SidebarHeaderComponent } from './dashboard/sidebar/sidebar-header/sidebar-header.component';
+import { SidebarItemSectionComponent } from './dashboard/sidebar/sidebar-item-section/sidebar-item-section.component';
+
+// others
+import { DocComponent } from './components/docs/doc/doc.component';
+import { ContentComponent } from './components/content/content.component';
+import { SnippetComponent } from './components/docs/snippet/snippet.component';
+import { FolderIconComponent } from './components/docs/icons/folder-icon/folder-icon.component';
+import { AngularIconComponent } from './components/docs/icons/angular-icon/angular-icon.component';
+import { FactureFormComponent } from './facture-form/facture-form.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+    // dashboard
+    LayoutComponent,
+    TopBarComponent,
+    OverlayComponent,
+    SidebarComponent,
+    SidebarItemComponent,
+    SidebarItemsComponent,
+    SidebarHeaderComponent,
+    SidebarItemSectionComponent,
+
+    // others
+    DocComponent,
+    SnippetComponent,
+    ContentComponent,
+    FolderIconComponent,
+    AngularIconComponent,
+    FactureFormComponent,
+    ContactComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
